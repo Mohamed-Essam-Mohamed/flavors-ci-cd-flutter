9e944912f7048892d031095395373f4a050a8100
mohamed <esammww.com@gmail.com>
2024-09-29 04:09:39 +0300

    setup flover android and run flover development and flover porduction.

---
+++ lib/main_prod.dart
@@ -0,0 +1,6 @@
+import 'package:flavers_flutter/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flavers_flutter/main.dart';
+import 'package:flavers_flutter/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
